5a74e52ffbaa290dd075780b68ea4ed2cfbe422e
ritikadhawan <ritikadhawan28@gmail.com>
2019-10-28 11:24:47 +0530

    make form and set view engine

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.urlencoded());
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+
 app.listen(port,(err)=>{
     if(err)
         console.log(`Error in ${port}`);
