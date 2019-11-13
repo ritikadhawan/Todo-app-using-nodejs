1fb194f85e543ea364e522c05c832d5b17574ab0
ritikadhawan <ritikadhawan28@gmail.com>
2019-11-13 20:38:39 +0530

    change in styling

--- static/js/script.js
+++ static/js/script.js
@@ -3,5 +3,14 @@ function displaySelectedTodo(element){
     document.getElementById('display-selected-todo').innerText = element.innerText;
 
 }
+// to view the add todo form 
+document.getElementById("add-todo-button").addEventListener('click',()=>{
+    document.getElementById('create-todo-form').style.display = "flex";
+});
+// for button to close the create todo form 
+document.getElementById('close-form').addEventListener('click',()=>{
+    document.getElementById('create-todo-form').style.display = "none";
+
+})
 
 
